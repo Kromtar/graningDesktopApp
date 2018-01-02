49df69d959270d03cee715b68ee254b522fcf7ac
Federico Jensen Carrasco <federicodaniel25@gmail.com>
2018-01-02 18:08:04 -0300

    Initial files

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const url = require('url');
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+function createWindow(){
+    mainWindow = new BrowserWindow({ width: 800, height: 600});
+    const startUrl = process.env.DEV_URL ||
+		url.format({
+		  pathname: path.join(__dirname, '/../build/index.html'),
+		  protocol: 'file:',
+		  slashes: true
+		});
+	mainWindow.loadURL(startUrl);
+
+    mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
